2fab3bd55115cdb42cedc989b77ab9acb3b0803b
tundrv0l <tundravol2@gmail.com>
2023-08-18 15:33:15 -0500

    LoadImage() is working

--- Project1/project01.cpp
+++ Project1/project01.cpp
@@ -6,52 +6,48 @@
 
 using namespace std;
 
-extern const int MAXROWS;               // Maximum number of rows in image
-extern const int MAXCOLS;                 // Maximum number of columns in image
+extern const int MAXROWS;               // Maximum number of rows in image, grabbed from main.cpp
+extern const int MAXCOLS;                 // Maximum number of columns in image, grabbed from main.cpp
 
 void LoadImage(string imagefile, int image[MAXROWS][MAXCOLS])
 {
-    for (int i = 0; i < MAXROWS; i++)
-    {
-        for (int j = 0; j < MAXCOLS; j++)
-        {
-            image[i][j] = 0;
-        }
-    }
+    /*
+    A function that loads the 10x10 image from the specified text file into a 2D array for processing.
 
-    std::cout << "File name: " << imagefile << std::endl;
+    Parameters
+    ----------
+    imagefile: STRING
+        The name of the file containing the image.
+    
+    image[MAXROWS][MAXCOLS]: INT
+        The given 2D array for storing the image.
+    */
 
     ifstream inputs;
     inputs.open(imagefile.c_str());
 
-    if (!inputs.is_open())
-    {
-        std::cout << "Unable to open file" << std::endl;
-        return;
-    }
-
     for (int i = 0; i < MAXROWS; i++)
     {
+        string line;
+        getline(inputs, line); // Read the entire line into a string
+
+        // Skip lines starting with '#', to ignore the header text
+        if (line.size() > 0 && line[0] == '#')
+        {
+            i--; // Go back and re-read this line
+            continue;
+        }
+
+        // Otherwise, read the line into the array
         for (int j = 0; j < MAXCOLS; j++)
         {
-            if (inputs.eof() || inputs.fail())
-            {
-                std::cout << "Reached end of file" << std::endl;
-                return;
-            }
 
             inputs >> image[i][j];
-            std::cout << i << " " << j << " " << image[i][j] << std::endl;  // josh debug
         }
     }
     inputs.close();
 }
 
-// Loads the image from the file into the array.
-// Parameters:
-//  imagefile - the name of the file containing the image
-//  image[MAXROWS][MAXCOLS] - the array for storing the image
-// Returns: nothing
 
 
 // The rest of the function prototypes go here
