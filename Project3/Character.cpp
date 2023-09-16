1374a8929661992a59b9ac980780d1a93926d781
tundrv0l <tundravol2@gmail.com>
2023-09-15 20:57:16 -0500

    fixed transformers

--- Project3/Character.cpp
+++ Project3/Character.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <string>
-#include <cstring> // Might remove, not sure how he wants this done
+#include <cstring>
 #include "Character.h"
 
 using namespace std;
@@ -12,7 +12,7 @@ using namespace std;
 Character::Character()
 // Default Constructor - Sets every private member of 'Character' to their unknown value (0 for ints) //
 {
-    strcpy_s(m_sName, "Unknown"); // Might remove, not sure how he wants this done
+    strcpy_s(m_sName, "Unknown");
     m_iClass = 0;
     m_iAlignment = 0;
     m_iHitPoints = 0;
@@ -23,11 +23,11 @@ Character::Character()
 }
 
 //Might change this array population as well
-Character::Character(char *name, int cl, int al, int hp, int str, int dex, int con, int itl, int wis, int chr) 
-: m_iCharTraits{ str, dex, con, itl, wis, chr }
+Character::Character(char *name, int cl, int al, int hp, int str, int dex, int cn, int itl, int wis, int chr) 
+: m_iCharTraits{ str, dex, cn, itl, wis, chr }
 // Parameterized Constructor - Sets every private member for 'Character' to their parameterized value //
 {
-    strcpy_s(m_sName, name); // Might remove, not sure how he wants this done
+    strcpy_s(m_sName, name); 
     m_iClass = cl;
     m_iAlignment = al;
     m_iHitPoints = hp;
@@ -59,4 +59,151 @@ A function that takes a class parameter, and sets it to a character class.
 */
 {
     m_iClass = cl;
+}
+
+void Character::setAlignment(int al)
+/*
+A function that takes an alignment parameter, and sets it to a character alignment.
+
+    Parameters
+    ----------
+    al: INT
+        An integer representing a 'alignment' archetype.
+*/
+{
+    m_iAlignment = al;
+}
+
+void Character::setHitPoints(int hp)
+/*
+A function that takes a hit point parameter, and sets it to a character hit point value.
+
+    Parameters
+    ----------
+    hp: INT
+        An integer representing a hit point value.
+*/
+{
+    m_iHitPoints = hp;
+}
+
+void Character::setStrength(int str)
+/*
+A function that takes a strength parameter, and sets it to the strength value index.
+
+    Parameters
+    ----------
+    str: INT
+        An integer representing a strength value.
+*/
+{
+    m_iCharTraits[0] = str;
+}
+
+void Character::setDexterity(int dex)
+/*
+A function that takes a dexterity parameter, and sets it to the dexterity value index.
+
+    Parameters
+    ----------
+    dex: INT
+        An integer representing a dexterity value.
+*/
+{
+    m_iCharTraits[1] = dex;
+}
+
+void Character::setConstitution(int cn)
+/*
+A function that takes a constitution parameter, and sets it to the constitution value index.
+
+    Parameters
+    ----------
+    cn: INT
+        An integer representing a constitution value.
+*/
+{
+    m_iCharTraits[2] = cn;
+}
+
+void Character::setIntelligence(int itl)
+/*
+A function that takes an intelligence parameter, and sets it to the intelligence value index.
+
+    Parameters
+    ----------
+    itl: INT
+        An integer representing an intelligence value.
+*/
+{
+    m_iCharTraits[3] = itl;
+}
+
+void Character::setWisdom(int wis)
+/*
+A function that takes a wisdom parameter, and sets it to the wisdom value index.
+
+    Parameters
+    ----------
+    wis: INT
+        An integer representing a wisdom value.
+*/
+{
+    m_iCharTraits[4] = wis;
+}
+
+void Character::setCharisma(int chr)
+/*
+A function that takes a charisma parameter, and sets it to the charisma value index.
+
+    Parameters
+    ----------
+    chr: INT
+        An integer representing a charisma value.
+*/
+{
+    m_iCharTraits[5] = chr;
+}
+
+// ** Character Observers ** //
+
+void Character::getName(char *name)
+/*
+A function that takes a name parameter, and copies it to a character array.
+
+    Parameters
+    ----------
+    *name: CHAR ARRAY
+        A pointer to a character array to copy our member array to.
+*/
+{
+    strcpy_s(name, 65, m_sName);
+}
+
+void Character::getClass(int &cl)
+/*
+A function that takes a class parameter, and copies it to a character class.
+
+    Parameters
+    ----------
+    cl: INT
+        An integer representing a 'class' archetype.
+*/
+{
+    cl = m_iClass;
+}
+
+
+// Idk if this is right tbh
+void Character::getAlignment(int &al)
+/*
+A function that takes an alignment parameter, and copies it to a character alignment.
+
+    Parameters
+    ----------
+    al: INT
+        An integer representing a 'alignment' archetype.
+*/
+{
+    al = m_iAlignment;
 }
