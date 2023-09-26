bdbcbfa78cf9865ac38fb595f75a118c1fcd637d
Parker Clark <tundravol2@gmail.com>
2023-09-26 11:59:12 -0500

    fixed defs, added note

--- Project3/CharacterList.h
+++ Project3/CharacterList.h
@@ -15,7 +15,7 @@ public:
     CharacterList(); // Default constructor
 
     // ** CharacterList Transformers ** //
-    bool addCharacter(Character *newCharacter); // Adds a character to the list
+    bool addCharacter(Character *newCharacter); // Adds a character to the list NOTE: add a sort functionality per req
     Character *deleteCharacter(char *characterName); // Removes a character from the list
     bool addItem(char *characterName, Item *newItem); // Adds an item to a character's inventory
     Item *dropItem(char *characterName, char *itemName); // Removes an item from a character's inventory
